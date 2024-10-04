ba77244d19dedbfeade512784e1e74381305f5a2
EMalpha01 <EM-alpha-01@proton.me>
2024-10-04 09:48:08 +0000

    namespaced.

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
+using namespace std;
+
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cout << "Hello, World!" << endl;
+    cout << "1" << endl;
+    cout << "2" << endl;
+    cout << "3" << endl;
+    
     return 0;
 }
